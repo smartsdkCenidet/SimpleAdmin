fb091603184b2c07dc000505c7238f994a285a02
danieltoo <torresestradadaniel@gmail.com>
2018-08-14 16:17:01 -0500

    i updated the smart url of devices 2

--- static/js/devices2.js
+++ static/js/devices2.js
@@ -4,12 +4,11 @@
 $('#zonelist2').change(function() {
     let idZone = $(this).val()
     //GET ALL INFORMATION OF A SPECIFIC CAMPUS
-    $.get("https://smartsecurity-webservice.herokuapp.com/api/zone/"+idZone, function(data){
+    $.get(`${smarService}/api/zone/${idZone}`, function(data){
         if(data.length===0){
             console.log("No se encontró información del campus");
         }
         else{
-            console.log(data);
             zoneLocation = data['location'];
             map.setView(new L.LatLng(data['centerPoint'][0], data['centerPoint'][1]), 18);
             polyline = L.polyline( data['location'], {color: '#ff6666'}).addTo(map);
@@ -22,7 +21,7 @@ function showDeviceOnMap(dataDevice){
     let locationTemp = dataDevice[0]['location'].split(",")
     map.setView(new L.LatLng(Number(locationTemp[0]), Number(locationTemp[1])), 19);
     polyline = L.polyline(zoneLocation).addTo(map);
-    fetch("https://smartsecurity-webservice.herokuapp.com/api/user?id="+dataDevice[0]['owner'], {
+    fetch(`${smarService}/api/user?id=${dataDevice[0]['owner']}`, {
         method: 'GET',
         headers: {
             'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, PATCH, DELETE'
@@ -30,7 +29,6 @@ function showDeviceOnMap(dataDevice){
     })
     .then((res) => res.json())
     .then((dataUser)=> {
-        console.dir(dataUser)
         if(dataUser){
             marker = L.marker(locationTemp, {
                 icon: L.MakiMarkers.icon({
@@ -43,7 +41,6 @@ function showDeviceOnMap(dataDevice){
             .openPopup()
             .addTo(markerLayer);
             markerLayer.addTo(map);
-            console.log("dos")
         }
     })
     .catch((error)=>{
@@ -52,9 +49,7 @@ function showDeviceOnMap(dataDevice){
 }
 function searching2(){
     let phone = $('#phonenumber-countrycode').val()+$('#input-search2').val();
-    console.log($("#zonelist2").val());
-    console.log(phone);
-    fetch("https://smartsecurity-webservice.herokuapp.com/service/devices/zone/"+$("#zonelist2").val()+"/owner?phoneNumber="+phone, {
+    fetch(`${smarService}/service/devices/zone/${$("#zonelist2").val()}/owner?phoneNumber=${phone}`, {
         method: 'GET',
         headers: {
             'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, PATCH, DELETE'
@@ -66,7 +61,6 @@ function searching2(){
             alert("This device has not been found in the zone");
         }  
         else{
-            console.dir(data) 
             showDeviceOnMap(data);
         }
     })
