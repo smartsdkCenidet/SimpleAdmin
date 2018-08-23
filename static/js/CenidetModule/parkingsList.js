7a96861fb4c7adf7783664a7044171a9e696a473
danieltoo <torresestradadaniel@gmail.com>
2018-08-23 18:00:35 -0500

    i updated load Datamodels

--- static/js/CenidetModule/parkingsList.js
+++ static/js/CenidetModule/parkingsList.js
@@ -1,53 +1,62 @@
 var allParkings = [];
 var allZones = [];
-//GET ALL ZONES REGISTERED
-$.get(`${smartService}/api/zone?status=1`, function(data){
-    if(data.length===0){
-        console.log("No se encontraron campus ");
-    }
-    else{
-        campus = data;
-        campus.forEach(element => {
-            allZones[element['idZone']] = element;
-        });
 
-    }
-});
-$.get(`${smartService}/api/parking?status=1`, function(data){
-    if(data.length===0){
-        console.log("No se encontraron campus ");
-    }
-    else{
-        campus = data;
-        campus.forEach(element => {
-            var zone = "";
-            if (allZones[element.areaServed] === undefined){
-                return;
-            }else{
-                zone = allZones[element.areaServed].name; 
-            }
-            $('#list').append(
-                '<tr>'+
-                '<td>' + element.idOffStreetParking.replace('OffStreetParking_','') + 
-                '</td><td>' + zone + 
-                '</td><td>' + element.name + 
-                '</td><td>' + element.description + '</td>'+
-                '<td>'+
-                `<a ref="#" id='delete${element.idOffStreetParking}'><i class="fas fa-trash"  ></i></a></td>`+
-                '</tr>');
-            allParkings[element['idOffStreetParking']] = element;
-
-            $(`#delete${element.idOffStreetParking}`).click(()=> {
-                if (confirm("Are you sure to delete this item? ")){
-                    deleteParking(element.idOffStreetParking);
-                }
-                
+load();
+async function loadZones () {
+    await $.get(`${smartService}/api/zone?status=1`, function(data){
+        if(data.length===0){
+            console.log("No se encontraron campus ");
+        }
+        else{
+            campus = data;
+            campus.forEach(element => {
+                allZones[element['idZone']] = element;
             });
-        });
 
-    }
-});
+        }
+    });
+}
+async function loadParkings (){
+    await $.get(`${smartService}/api/parking?status=1`, function(data){
+        if(data.length===0){
+            console.log("No se encontraron campus ");
+        }
+        else{
+            campus = data;
+            campus.forEach(element => {
+                var zone = "";
+                if (allZones[element.areaServed] === undefined){
+                    return;
+                }else{
+                    zone = allZones[element.areaServed].name; 
+                }
+                $('#list').append(
+                    '<tr>'+
+                    '<td>' + element.idOffStreetParking.replace('OffStreetParking_','') + 
+                    '</td><td>' + zone + 
+                    '</td><td>' + element.name + 
+                    '</td><td>' + element.description + '</td>'+
+                    '<td>'+
+                    `<a ref="#" id='delete${element.idOffStreetParking}'><i class="fas fa-trash"  ></i></a></td>`+
+                    '</tr>');
+                allParkings[element['idOffStreetParking']] = element;
 
+                $(`#delete${element.idOffStreetParking}`).click(()=> {
+                    if (confirm("Are you sure to delete this item? ")){
+                        deleteParking(element.idOffStreetParking);
+                    }
+                    
+                });
+            });
+
+        }
+    });
+}
+
+async function load() {
+    await loadZones();
+    await loadParkings();
+}
 
 function deleteParking (id) {
     console.log(`${smartService}/api/parking/${id}`)
--- static/js/CenidetModule/roadSegmentsList.js
+++ static/js/CenidetModule/roadSegmentsList.js
@@ -1,56 +1,67 @@
 var allRoads = {};
-$.get(`${smartService}/api/road?status=1`, function(data){
-    if(data.length===0){
-        console.log("No se encontraron campus ");
-    }
-    else{
-        roads = data;
-        roads.forEach(element => {
-            allRoads[element['idRoad']] = element;
-        });
 
-    }
-});
+load();
 
-$.get(`${smartService}/api/roadSegment?status=1`, function(data){
-    console.log(allRoads)
-    if(data.length===0){
-        console.log("No se encontraron roadSegments ");
-    }
-    else{
-        roads = data;
-        roads.forEach(element => {
-            var refRoad = element.refRoad;
-            console.log(allRoads[element.refRoad] === undefined)
-            if (allRoads[element.refRoad] === undefined)
-                return;
-            else 
-                refRoad = allRoads[element.refRoad].name;
-
-            $('#list').append(
-                '<tr>'+
-                '<td>' + element.idRoadSegment.replace('RoadSegment_','') + 
-                '</td><td>' + refRoad+ 
-                '</td><td>' + element.name + 
-                '</td><td>' + element.laneUsage.join(",")+ '</td>'+
-                '</td><td>' + "Max :"+ element.maximumAllowedSpeed + ",Min :"+ element.minimumAllowedSpeed + '</td>'+
-                '</td><td>' + element.width+ '</td>'+                
-                '<td>'+
-                `<a ref="#" id='delete${element.idRoadSegment}'><i class="fas fa-trash"  ></i></a></td>`+
-                '</tr>');
-            allRoads[element['idRoadSegment']] = element;
-
-            $(`#delete${element.idRoadSegment}`).click(()=> {
-                if (confirm("Are you sure to delete this item?")){
-                    deleteRoadSegment(element.idRoadSegment);
-                }
-                
+async function loadRoads () {
+    await $.get(`${smartService}/api/road?status=1`, function(data){
+        if(data.length===0){
+            console.log("No se encontraron campus ");
+        }
+        else{
+            roads = data;
+            roads.forEach(element => {
+                allRoads[element['idRoad']] = element;
             });
-        });
 
-    }
-});
+        }
+    });
+}
 
+async function loadRoadSegments() {
+    await $.get(`${smartService}/api/roadSegment?status=1`, function(data){
+        console.log(allRoads)
+        if(data.length===0){
+            console.log("No se encontraron roadSegments ");
+        }
+        else{
+            roads = data;
+            roads.forEach(element => {
+                var refRoad = element.refRoad;
+                console.log(allRoads[element.refRoad] === undefined)
+                if (allRoads[element.refRoad] === undefined)
+                    return;
+                else 
+                    refRoad = allRoads[element.refRoad].name;
+
+                $('#list').append(
+                    '<tr>'+
+                    '<td>' + element.idRoadSegment.replace('RoadSegment_','') + 
+                    '</td><td>' + refRoad+ 
+                    '</td><td>' + element.name + 
+                    '</td><td>' + element.laneUsage.join(",")+ '</td>'+
+                    '</td><td>' + "Max :"+ element.maximumAllowedSpeed + ",Min :"+ element.minimumAllowedSpeed + '</td>'+
+                    '</td><td>' + element.width+ '</td>'+                
+                    '<td>'+
+                    `<a ref="#" id='delete${element.idRoadSegment}'><i class="fas fa-trash"  ></i></a></td>`+
+                    '</tr>');
+                allRoads[element['idRoadSegment']] = element;
+
+                $(`#delete${element.idRoadSegment}`).click(()=> {
+                    if (confirm("Are you sure to delete this item?")){
+                        deleteRoadSegment(element.idRoadSegment);
+                    }
+                    
+                });
+            });
+
+        }
+    });
+}
+
+async function load () {
+    await loadRoads();
+    await loadRoadSegments();
+}
 
 function deleteRoadSegment (id) {
     console.log(`ejecutando ..`)
